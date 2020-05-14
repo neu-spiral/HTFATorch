d2dfc3491ca2c599e70310087bdfa64822c5032a
zqkhan <khanzu@ece.neu.edu>
2020-05-14 16:27:44 -0400

    necessary changes for model 15

--- htfa_torch/dtfa.py
+++ htfa_torch/dtfa.py
@@ -34,7 +34,7 @@ import torch.nn as nn
 from torch.nn import Parameter
 from torch.nn.functional import softplus
 import torch.optim.lr_scheduler
-
+import itertools
 import probtorch
 
 from . import dtfa_models
@@ -77,9 +77,12 @@ class DeepTFA:
 
         subjects = self.subjects()
         tasks = self.tasks()
+        self.num_tasks = len(tasks)
+        interactions = OrderedSet(list(itertools.product(subjects,tasks)))
         block_subjects = [subjects.index(b.subject) for b in self._blocks]
         block_tasks = [tasks.index(b.task) for b in self._blocks]
-
+        block_interactions = [interactions.index((b.subject,b.task))
+                              for b in self._blocks]
         b = max(range(self.num_blocks), key=lambda b: self.num_times[b])
         init_activations = self.voxel_activations.copy()
         max_times = max(self.num_times)
@@ -99,14 +102,16 @@ class DeepTFA:
         }
 
         self.decoder = dtfa_models.DeepTFADecoder(self.num_factors, hyper_means,
+                                                  self.num_tasks,
                                                   embedding_dim,
                                                   time_series=model_time_series)
         self.generative = dtfa_models.DeepTFAModel(
-            self.voxel_locations, block_subjects, block_tasks,
+            self.voxel_locations, block_subjects, block_tasks , block_interactions,
             self.num_factors, self.num_blocks, self.num_times, embedding_dim
         )
         self.variational = dtfa_models.DeepTFAGuide(self.num_factors,
                                                     block_subjects, block_tasks,
+                                                    block_interactions,
                                                     self.num_blocks,
                                                     self.num_times,
                                                     embedding_dim, hyper_means,
@@ -357,7 +362,7 @@ class DeepTFA:
                  prior_kl.mean(dim=0).item()],
                 [iwae_free_energy, iwae_log_likelihood, iwae_prior_kl]]
 
-    def results(self, block=None, subject=None, task=None, hist_weights=False):
+    def results(self, block=None, subject=None, task=None, interaction=None, hist_weights=False):
         hyperparams = self.variational.hyperparams.state_vardict()
         for k, v in hyperparams.items():
             hyperparams[k] = v.expand(1, *v.shape)
@@ -370,11 +375,13 @@ class DeepTFA:
             blocks = [block]
             block_subjects = [self.generative.block_subjects[block]]
             block_tasks = [self.generative.block_tasks[block]]
+            block_interactions = [self.generative.block_interactions[block]]
         else:
             times = (0, max(self.num_times))
             blocks = []
             block_subjects = self.generative.block_subjects
             block_tasks = self.generative.block_tasks
+            block_interactions = self.generative.block_interactions
 
         for b in blocks:
             if subject is not None:
@@ -401,13 +408,13 @@ class DeepTFA:
                     value=factor_log_widths_params['mu'][:, subject],
                     name='FactorLogWidths%d' % b,
                 )
-            if task is not None:
+            if interaction is not None:
                 guide.variable(
                     torch.distributions.Normal,
-                    hyperparams['task']['mu'][:, task],
-                    softplus(hyperparams['task']['sigma'][:, task]),
-                    value=hyperparams['task']['mu'][:, task],
-                    name='z^S_{%d,%d}' % (task, b),
+                    hyperparams['interactions']['mu'][:, task],
+                    softplus(hyperparams['interactions']['sigma'][:, task]),
+                    value=hyperparams['interactions']['mu'][:, task],
+                    name='z^I_{%d,%d}' % (interaction , b),
                 )
             if self._time_series:
                 for k, v in hyperparams['weights'].items():
@@ -422,7 +429,7 @@ class DeepTFA:
                 )
 
         weights, factor_centers, factor_log_widths =\
-            self.decoder(probtorch.Trace(), blocks, block_subjects, block_tasks,
+            self.decoder(probtorch.Trace(), blocks, block_subjects, block_tasks, block_interactions,
                          hyperparams, times, guide=guide, num_particles=1)
 
         if block is not None:
@@ -447,8 +454,8 @@ class DeepTFA:
         }
         if subject is not None:
             result['z^P_%d' % subject] = hyperparams['subject']['mu'][:, subject]
-        if task is not None:
-            result['z^S_%d' % task] = hyperparams['task']['mu'][:, task]
+        if interaction is not None:
+            result['z^I_{%d}' % interaction] = hyperparams['interactions']['mu'][:, interaction]
         return result
 
     def reconstruction(self, block=None, subject=None, task=None, t=0):
@@ -912,6 +919,54 @@ class DeepTFA:
             torch.save(tensors, tensors_filename)
 
         utils.embedding_clusters_fig(z_p_mu, z_p_sigma, subject_colors, 'z^P',
+                                     'Participant Embeddings', palette,
+                                     filename=filename, show=show, xlims=xlims,
+                                     ylims=ylims, figsize=figsize,
+                                     plot_ellipse=plot_ellipse,
+                                     legend_ordering=legend_ordering)
+    def scatter_subject_weight_embedding(self, labeler=None, filename='', show=True,
+                                  xlims=None, ylims=None, figsize=utils.FIGSIZE,
+                                  colormap=plt.rcParams['image.cmap'],
+                                  serialize_data=True, plot_ellipse=True,
+                                  legend_ordering=None):
+        if filename == '':
+            filename = self.common_name() + '_subject_weight_embedding.pdf'
+        hyperparams = self.variational.hyperparams.state_vardict()
+        z_p_mu = hyperparams['subject_weight']['mu'].data
+        z_p_sigma = softplus(hyperparams['subject_weight']['sigma'].data)
+        subjects = self.subjects()
+
+        minus_lims = torch.min(z_p_mu - z_p_sigma * 2, dim=0)[0].tolist()
+        plus_lims = torch.max(z_p_mu + z_p_sigma * 2, dim=0)[0].tolist()
+        if not xlims:
+            xlims = (minus_lims[0], plus_lims[0])
+        if not ylims:
+            ylims = (minus_lims[1], plus_lims[1])
+
+        if labeler is None:
+            labeler = lambda s: s
+        labels = sorted(list({labeler(s) for s in subjects}))
+        if all([isinstance(label, float) for label in labels]):
+            palette = cm.ScalarMappable(None, colormap)
+            subject_colors = palette.to_rgba(np.array(labels), norm=True)
+            palette.set_array(np.array(labels))
+        else:
+            palette = dict(zip(
+                labels, utils.compose_palette(len(labels), colormap=colormap)
+            ))
+            subject_colors = [palette[labeler(subject)] for subject in subjects]
+
+        if serialize_data:
+            tensors_filename = os.path.splitext(filename)[0] + '.dat'
+            tensors = {
+                'z_pw': {'mu': z_p_mu, 'sigma': z_p_sigma},
+                'palette': palette,
+                'subject_colors': subject_colors,
+                'labels': labels,
+            }
+            torch.save(tensors, tensors_filename)
+
+        utils.embedding_clusters_fig(z_p_mu, z_p_sigma, subject_colors, 'z^PW',
                                      'Participant Embeddings', palette,
                                      filename=filename, show=show, xlims=xlims,
                                      ylims=ylims, figsize=figsize,
